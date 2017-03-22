625b2da6952cec9167bec0f5d7f6c8d731706b11
ran <abbshr@outlook.com>
2017-03-22 21:12:29 +0800

    mem cpu io

---
+++ gather.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+PID=${PID:-$$}
+PROC=/proc/$PID
+
+COLLECTOR_ADDR="http://192.168.13.157:8080"
+REPORT_TEMPLATE='{ "rss": %d, "read_bytes": %d, "write_bytes": %d, "cpu_usage": %d }'
+REPORT_HEADER="Content-Type: application/json"
+REPORT_URL=$COLLECTOR_ADDR/
+FREQUENCY=1s
+
+declare -i PROCESSORS
+declare -i TOTAL_CPUTIME
+declare -i CPUTIME
+PROCESSORS=$(processors)
+TOTAL_CPUTIME=$(totalcputime)
+CPUTIME=$(cputime)
+
+# total memory (KB)
+total_memory() {
+  cat /proc/meminfo | grep "MemTotal" | cut -d":" -f2 | xargs echo | cut -d" " -f1
+}
+
+processors() {
+  cat /proc/cpuinfo | grep "siblings" | wc -l
+}
+
+# utime nice systime idle iowait irq softirq steal guest guest_nice (jiffies)
+totalcpustat() {
+  local raw=($(head -n1 /proc/stat))
+  echo ${raw[@]:1}
+}
+
+accumulate() {
+  read -a arr
+  local -i sum
+  for t in ${arr[@]}; do
+    sum+=$t
+  done
+  echo sum
+}
+
+# utime stime cutime cstime (jiffies)
+cpustat() {
+  local raw=$(cat $PROC/stat)
+  echo ${raw[*]:13:4}
+}
+
+totalcputime() {
+  totalcpustat | accumulate
+}
+
+cputime() {
+  cpustat | accumulate
+}
+
+cpu() {
+  local -i curr_cputime
+  local -i curr_total_cputime
+  curr_cputime=$(cputime)
+  curr_total_cputime=$(totalcputime)
+  echo $(( ($curr_cputime - $CPUTIME) * $PROCESSORS / ($curr_total_cputime - $TOTAL_CPUTIME) ))
+  CPUTIME=$curr_cputime
+  TOTAL_CPUTIME=$curr_total_cputime
+}
+
+# ret: rss (KB)
+rss() {
+  cat $PROC/status | grep "VmRSS" | cut -d":" -f2 | xargs echo | cut -d" " -f1
+}
+
+# ret: read write (B)
+io() {
+  cat $PROC/io | grep "^read_bytes\|^write_bytes" | cut -d":" -f2 | xargs echo
+}
+
+gather_facts() {
+  printf "$REPORT_TEMPLATE" $(rss) $(io) $(cpu)
+}
+
+report() {
+  read body
+  curl -X POST -H "$REPORT_HEADER" -d "$body" $REPORT_URL
+}
+
+while true; do
+  gather_facts | report
+  sleep $FREQUENCY
+done
